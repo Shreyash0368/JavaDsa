501d1aea6fa6aab3f3f7c7b20693a438a5f5d830
Shreyash0368 <shreyashgpt0503@gmail.com>
2023-08-14 13:46:07 +0530

    rod cutting

--- DP_46.java
+++ DP_46.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class DP_46 {
-        public static int coinChange(int num[], int tar) {
+    public static int coinChange(int num[], int tar) {
         int[][] dp = new int[num.length][tar + 1];
         for (int i = 0; i < dp.length; i++) {
             Arrays.fill(dp[i], -1);
@@ -53,6 +53,49 @@ public class DP_46 {
         }
 
         return (dp[num.length - 1][tar] >= (int) 1e9 ? -1 : dp[num.length - 1][tar]);
+    }
+
+    public static int rodCut(int[] price, int n) {
+        int[][] dp = new int[n][n + 1];
+        for (int i = 0; i < dp.length; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return rodCutUtil(price, dp, n, n);
+
+    }
+
+    private static int rodCutUtil(int[] price, int[][] dp, int n, int len) {
+        if (len == 0)
+            return dp[n - 1][len] = 0;
+        if (n == 1)
+            return dp[n - 1][len] = price[0] * len;
+
+        if (dp[n - 1][len] != -1)
+            return dp[n - 1][len];
+
+        int notCut = 0 + rodCutUtil(price, dp, n - 1, len);
+        int cut = Integer.MIN_VALUE;
+        if (n <= len)
+            cut = price[n - 1] + rodCutUtil(price, dp, n, len - n);
+
+        return dp[n - 1][len] = Math.max(cut, notCut);
+    }
+
+    public static int rodCutTabulation(int[] price, int len) {
+        int[][] dp = new int[len + 1][len + 1];
+
+        for (int n = 1; n < dp.length; n++) {
+            for (int k = 1; k < dp[n].length; k++) {
+                int notCut = 0 + dp[n - 1][k];
+                int cut = Integer.MIN_VALUE;
+                if (n <= k)
+                    cut = price[n - 1] + dp[n][k - n];
+
+                dp[n][k] = Math.max(notCut, cut);
+            }
+        }
+
+        return dp[len][len];
     }
 
 }
